925d2f0a2133020ce91f63825522d5f50b0f4785
Aashish Dhawan <aashishdhawan@gmail.com>
2026-03-16 22:19:48 +0530

    replace tippy

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,16 +1,36 @@
 import { Controller } from "@hotwired/stimulus";
-import tippy from "tippy.js";
 
 export default class extends Controller {
   connect() {
-    tippy(this.element, {
-      allowHTML: true,
-      theme: 'material',
-      animation: 'fade',
-      arrow: true,
-      offset: [20, 5],
-      content: this.element.getAttribute("data-tooltip-content"),
-      trigger: 'mouseenter'
-    });
+    this.content = this.element.getAttribute("data-tooltip-content");
+    if (!this.content) return;
+
+    this.tooltip = document.createElement("div");
+    this.tooltip.className = "tooltip-popup";
+    this.tooltip.innerHTML = this.content;
+
+    this.showHandler = this.show.bind(this);
+    this.hideHandler = this.hide.bind(this);
+    this.element.addEventListener("mouseenter", this.showHandler);
+    this.element.addEventListener("mouseleave", this.hideHandler);
+  }
+
+  disconnect() {
+    this.element.removeEventListener("mouseenter", this.showHandler);
+    this.element.removeEventListener("mouseleave", this.hideHandler);
+    this.hide();
+  }
+
+  show() {
+    document.body.appendChild(this.tooltip);
+    const rect = this.element.getBoundingClientRect();
+    this.tooltip.style.top = `${rect.top + window.scrollY - this.tooltip.offsetHeight - 6}px`;
+    this.tooltip.style.left = `${rect.left + window.scrollX + rect.width / 2 - this.tooltip.offsetWidth / 2}px`;
+    this.tooltip.classList.add("visible");
+  }
+
+  hide() {
+    this.tooltip.classList.remove("visible");
+    if (this.tooltip.parentNode) this.tooltip.parentNode.removeChild(this.tooltip);
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.13/src/index.js"
 
-pin "tippy.js", to: "https://ga.jspm.io/npm:tippy.js@6.3.7/dist/tippy.esm.js"
-
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@4.1.0/index.js"
