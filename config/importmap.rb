1d2f5042b66e70bbb7bfa31058838047539cc5cc
Aashish Dhawan <aashishdhawan@gmail.com>
2026-04-06 13:21:07 +0530

    make requestjs local

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "application"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.13/src/index.js"
+pin "@rails/request.js", to: "request.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 
