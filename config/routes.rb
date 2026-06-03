6c19d692e83b44941a3355d8c53ef9d1ab6b4372
Aashish Dhawan <aashishdhawan@gmail.com>
2026-06-03 10:24:06 +0530

    remove signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get    "login",  to: "sessions#new",     as: :login
   post   "login",  to: "sessions#create"
   delete "logout", to: "sessions#destroy", as: :logout
-  get    "signup", to: "registrations#new", as: :signup
-  post   "signup", to: "registrations#create"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
