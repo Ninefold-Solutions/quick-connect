c357c2a901c49c5d9c319c04951bcf2d248d88fd
Aashish Dhawan <aashishdhawan@gmail.com>
2026-06-03 10:38:26 +0530

    remove action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # config.active_job.queue_name_prefix = "recruit_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -88,7 +88,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "connect.besttalent.io" }
+  # config.action_mailer.default_url_options = { host: "connect.besttalent.io" }
 
   config.default_url_options = { host: "connect.besttalent.io" }
   
