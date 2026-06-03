268d06147fda588f9e0eaaa20426c0d4419ab898
Aashish Dhawan <aashishdhawan@gmail.com>
2026-06-03 10:38:45 +0530

    url fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "ats.besttalent.io"     # Allow requests from the production domain
+    "connect.besttalent.io"     # Allow requests from the production domain
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
