31b72cab353b0b6881d1e707497da3fe7882c103
Aashish Dhawan <aashishdhawan@gmail.com>
2026-08-08 12:25:18 +0530

    UI fix

--- test/system/contact/timeline_test.rb
+++ test/system/contact/timeline_test.rb
@@ -1,4 +1,4 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class ContactTimelineTest < ApplicationSystemTestCase
   setup do
@@ -13,15 +13,15 @@ class ContactTimelineTest < ApplicationSystemTestCase
     contact_timeline_index_url(script_name: "/#{@account.id}", contact_id: @contact.id)
   end
 
-  test "can visit index page if logged in" do
+  test 'can visit index page if logged in' do
     visit page_url
     take_screenshot
-    assert_selector "p", text: "Contact Timeline"
+    assert_selector 'p', text: 'Contact Timeline'
   end
 
-  test "can not visit index if not logged in" do
+  test 'can not visit index if not logged in' do
     sign_out @user
     visit page_url
-    assert_selector "h1", text: "Sign in to your account"
+    assert_selector 'h1', text: 'Sign in to your account'
   end
 end
