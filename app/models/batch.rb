77444903ca8261c3d1c3f8957e26188b9b5156fb
Aashish Dhawan <aashishdhawan@gmail.com>
2026-08-08 10:24:49 +0530

    batch validations

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -10,13 +10,17 @@ class Batch < ApplicationRecord
     contracts: 7
   }, default: :archive
 
+  validates_presence_of :name
   validates_uniqueness_to_tenant :name, case_sensitive: false
+  validates_uniqueness_to_tenant :website, case_sensitive: false, allow_blank: true
+  validates :people_count, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+
   has_many :batches_contacts
   has_and_belongs_to_many :contacts, touch: true
-  normalizes :name, with: ->(value) { value&.strip }
-  validates_presence_of :name
-  validates :people_count, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   has_many :events, class_name: 'Event', foreign_key: 'trackable', dependent: :destroy
+
+  normalizes :name, with: ->(value) { value&.strip }
+  normalizes :website, with: ->(value) { value&.strip }
 
   def self.query(params, includes = nil)
     return [] if params.empty?
